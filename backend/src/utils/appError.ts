d243728b09b1fab50f06b30829c9b24f90f75c03
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-02-28 19:25:57 +0530

    feat: extend AppError class with HttpException and InternalServerException for improved error handling

--- backend/src/utils/appError.ts
+++ backend/src/utils/appError.ts
@@ -1,18 +1,41 @@
 import { HTTPSTATUS, HttpStatusCodeType } from "../config/http.config";
-import { ErrorCodeEnumType } from "../enums/error-code.enum";
+import { ErrorCodeEnum, ErrorCodeEnumType } from "../enums/error-code.enum";
 
 export class AppError extends Error {
   public statusCode: HttpStatusCodeType | undefined;
   public errorCode?: ErrorCodeEnumType;
 
   constructor(
-    message: string, 
+    message: string,
     statusCode = HTTPSTATUS.INTERNAL_SERVER_ERROR,
-    errorCode?:ErrorCodeEnumType
-){
+    errorCode?: ErrorCodeEnumType
+  ) {
     super(message);
     this.statusCode = statusCode;
     this.errorCode = errorCode;
     Error.captureStackTrace(this, this.constructor);
+  }
 }
+
+export class HttpException extends AppError {
+  constructor(
+    message = "Http Exception Error",
+    statusCode: HttpStatusCodeType,
+    errorCode?: ErrorCodeEnumType
+  ) {
+    super(message, statusCode, errorCode);
+  }
+}
+
+export class InternalServerException extends AppError {
+  constructor(
+    message = "Internal Server Error",
+    errorCode?: ErrorCodeEnumType
+  ) {
+    super(
+      message,
+      HTTPSTATUS.INTERNAL_SERVER_ERROR,
+      errorCode || ErrorCodeEnum.INTERNAL_SERVER_ERROR
+    );
+  }
 }
