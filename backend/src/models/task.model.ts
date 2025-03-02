24fc42fb5db12c3f9ef355bec29de7d42f34c168
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-03-02 20:49:02 +0530

    feat: enhance Task model with additional fields and create Member model for workspace membership management

--- backend/src/models/task.model.ts
+++ backend/src/models/task.model.ts
@@ -1,6 +1,11 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { TaskPriorityEnumType, TaskStatusEnumType } from "../enums/task.enum";
-import { generateInviteCode, generateTaskCode } from "../utils/uuid";
+import {
+  TaskPriorityEnum,
+  TaskPriorityEnumType,
+  TaskStatusEnum,
+  TaskStatusEnumType,
+} from "../enums/task.enum";
+import { generateTaskCode } from "../utils/uuid";
 
 export interface TaskDocument extends Document {
   taskCode: string;
@@ -17,10 +22,63 @@ export interface TaskDocument extends Document {
   updatedAt: Date;
 }
 
-const taskSchema = new Schema<TaskDocument>({
-  taskCode: {
-    type: String,
-    unique: true,
-    default: generateTaskCode,
+const taskSchema = new Schema<TaskDocument>(
+  {
+    taskCode: {
+      type: String,
+      unique: true,
+      default: generateTaskCode,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    workspace: {
+      type: Schema.Types.ObjectId,
+      ref: "Workspace",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(TaskStatusEnum),
+      default: TaskStatusEnum.TODO,
+    },
+    priority: {
+      type: String,
+      enum: Object.values(TaskPriorityEnum),
+      default: TaskPriorityEnum.MEDIUM,
+    },
+    assignedTo: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      default: null,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const TaskModel = mongoose.model<TaskDocument>("Task", taskSchema);
+
+export default TaskModel;
