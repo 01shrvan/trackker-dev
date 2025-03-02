115622252bf31719668fb949a4404ad2945915c2
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-03-02 21:14:00 +0530

    feat: create Role model with schema and permissions mapping

--- backend/src/models/roles-permission.model.ts
+++ backend/src/models/roles-permission.model.ts
@@ -7,6 +7,32 @@ import {
 } from "../enums/role.enum";
 import { RolePermissions } from "../utils/role-permission";
 
-export interface RolesPermissionDocument extends Document {
+export interface RoleDocument extends Document {
   name: RoleType;
+  permissions: Array<PermissionType>;
 }
+
+const roleSchema = new Schema<RoleDocument>(
+  {
+    name: {
+      type: String,
+      enum: Object.values(Roles),
+      required: true,
+      unique: true,
+    },
+    permissions: {
+      type: [String],
+      enum: Object.values(Permissions),
+      required: true,
+      default: function (this: RoleDocument) {
+        return RolePermissions[this.name];
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const RoleModel = mongoose.model<RoleDocument>("Role", roleSchema);
+export default RoleModel;
